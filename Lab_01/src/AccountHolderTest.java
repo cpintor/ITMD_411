565da0e83b0dab2665495f38f417fd67faaf10c5
Cristian Pintor <cpintor@hawk.iit.edu>
2020-02-06 23:42:23 -0600

    Adding date

--- Lab_01/src/AccountHolderTest.java
+++ Lab_01/src/AccountHolderTest.java
@@ -1,15 +1,31 @@
 /*
-* 
+*
 * Author: Cristian Pintor
 * Course: ITMD 411
 * Description: Bank program for depositing and withdrawing money.
 *
 * */
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class AccountHolderTest {
     public static void main(String[] args) {
         System.out.println("C. Pintor | Lab 01");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+
+        // Create scanner object
+        Scanner input = new Scanner(System.in);
+        // Asking user for balance input
+        System.out.println("Please enter your account balance: ");
+        // Create numerical input
+        double balance = input.nextDouble();
+
+        // Output
+        System.out.println("Balance: " + balance);
+
     }
 }
