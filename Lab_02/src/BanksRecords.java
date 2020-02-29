9179081cc1a11b249302f0512adfb8f42347250f
Cristian Pintor <cpintor@hawk.iit.edu>
2020-02-29 14:53:56 -0600

    Adding rest of getters and setters

--- Lab_02/src/BanksRecords.java
+++ Lab_02/src/BanksRecords.java
@@ -2,7 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BanksRecords {
+    // setup static objects for IO processing
+
+    // array of BankRecords objects
     static BanksRecords robjs[] = new BanksRecords[600];
+    // arrayList to hold spreadsheet
     static ArrayList<List<String>> array = new ArrayList<>();
 
     // instance fields
@@ -17,4 +21,93 @@ public class BanksRecords {
     private String current_act;
     private String mortgage;
     private String pep;
+
+    // accessors and muttators
+    public String getId(){
+        return this.id;
+    }
+
+    public void setId(String i_d){
+        this.id = i_d;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setAge(int a){
+        this.age = a;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String _sex) {
+        this.sex = _sex;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double _income) {
+        this.income = _income;
+    }
+
+    public String getMarried() {
+        return married;
+    }
+
+    public void setMarried(String _married) {
+        this.married = _married;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int _children) {
+        this.children = _children;
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String _car) {
+        this.car = _car;
+    }
+
+    public String getSave_act() {
+        return save_act;
+    }
+
+    public void setSave_act(String _save_act) {
+        this.save_act = _save_act;
+    }
+
+    public String getCurrent_act() {
+        return current_act;
+    }
+
+    public void setCurrent_act(String _current_act) {
+        this.current_act = _current_act;
+    }
+
+    public String getMortgage() {
+        return mortgage;
+    }
+
+    public void setMortgage(String _mortgage) {
+        this.mortgage = _mortgage;
+    }
+
+    public String getPep() {
+        return pep;
+    }
+
+    public void setPep(String _pep) {
+        this.pep = _pep;
+    }
 }
