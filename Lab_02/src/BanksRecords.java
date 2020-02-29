fbd1db90bca1b7ef978f2f2ce5433d65e882ad46
Cristian Pintor <cpintor@hawk.iit.edu>
2020-02-28 19:55:55 -0600

    Adding more variables

--- Lab_02/src/BanksRecords.java
+++ Lab_02/src/BanksRecords.java
@@ -8,4 +8,13 @@ public class BanksRecords {
     // instance fields
     private String id;
     private int age;
+    private String sex;
+    private double income;
+    private String married;
+    private int children;
+    private String car;
+    private String save_act;
+    private String current_act;
+    private String mortgage;
+    private String pep;
 }
